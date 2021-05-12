88b4d90fdc15526162e432ba480f6a7bb467b8b5
wang.xander <wang.xander@gmail.com>
2021-05-12 15:31:11 -0700

    Added three entries that sum to 2020

--- day3_grade2_functionChallenge/sum2020.py
+++ day3_grade2_functionChallenge/sum2020.py
@@ -3,7 +3,7 @@ PUZZLES_DIR = "./day3_grade2_functionChallenge/puzzles.txt"
 TARGET = 2020 # target value =? key + value 
 
 
-def findTwoPuzzles():
+def findTwoPuzzles(target):
     result = None;
     dictionaryPuzzles = {}
     try:
@@ -11,25 +11,42 @@ def findTwoPuzzles():
         with open(PUZZLES_DIR, 'r') as puzzles:
             for puzzle in puzzles:
                 if puzzle.strip().isdigit():
-                    if pairPuzzles(dictionaryPuzzles, int(puzzle)):
-                        result = int(puzzle) * (TARGET - int(puzzle))
+                    if pairPuzzles(dictionaryPuzzles, int(puzzle), target):
+                        result = int(puzzle) * (target - int(puzzle))
                         break # the answer has only one pair
                     else:
                         continue
-        if result:
-            print(f"Found answer: {result}")
-        else:
-            print(f"Cannot find two entries that sum to {TARGET}")
+        return result
     except:
         print("Something bad happened")
 
 # if locate the correct pair of values return true, else false 
-def pairPuzzles(dictionary, value):
-    print(value)
+def pairPuzzles(dictionary, value, target):
     if value in dictionary:
         return True
     else:
-        dictionary[TARGET-value] = value 
+        dictionary[target-value] = value 
         return False
 
-findTwoPuzzles()
+# just a quick solution
+# need more work
+def findThreePuzzles():
+    try:
+        allValues = []
+        with open(PUZZLES_DIR, 'r') as puzzles:
+            allValues = puzzles.readlines() # saved in memory; not good for big file
+        # loop through all values and use TARGET - value as a target in two puzzles function findTwoPuzzles
+        for value in allValues:
+            answer = findTwoPuzzles(TARGET - int(value))
+            if answer:
+                return answer * int(value)
+        
+    except:
+        print("something bad happend in three puzzels")
+
+
+answer = findTwoPuzzles(TARGET)
+print(f"2 puzzels: The answer is {answer}")
+
+answer = findThreePuzzles()
+print(f"3 puzzels: The answer is {answer}")
