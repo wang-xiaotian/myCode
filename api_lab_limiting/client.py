33317dc9486f8a81fa32a757a8e5bb07d9417155
wang.xander <wang.xander@gmail.com>
2021-05-19 14:35:55 -0700

    merged

--- api_lab_limiting/client.py
+++ api_lab_limiting/client.py
@@ -1,11 +1,6 @@
 import requests
 
 URL = 'http://0.0.0.0:2224/fast'
-<<<<<<< HEAD
-response = requests.get(URL)
-print(response.text)
-=======
 for i in range(201):
     response = requests.get(URL)
     print(f'#{i} fast request: {response.text}')
->>>>>>> d419e641e2e37dd80a76d2fe924a785794a9d0d3
