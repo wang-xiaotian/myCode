d4e635307b3b276b96bc5ecd5233aaf53310d199
wang.xander <wang.xander@gmail.com>
2021-05-21 07:33:23 -0700

    Learning tensorflow

--- learning_tensorFlow/beginner.py
+++ learning_tensorFlow/beginner.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 '''
 beginner tensorflow project
+https://www.tensorflow.org/tutorials/keras/classification
+
+1. Define the training data
+2. Define a neural network model
+3. Configure the learning process
+4. Train the model
 '''
-# tensorflow
+# tensorflow library
 import tensorflow as tf
 
 # helper libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
+# tensorflow 1.0 vs 2.0
 print(tf.__version__)
+
+# label a image by one of the class names
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# keras learning dataset
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 def plot_image(i, predictions_array, true_label, img):
     true_label, img = true_label[i], img[i]
@@ -41,108 +56,106 @@ def plot_value_array(i, predictions_array, true_label):
     thisplot[predicted_label].set_color('red')
     thisplot[true_label].set_color('blue')
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-fashion_mnist = tf.keras.datasets.fashion_mnist
+def plotAll(predictions):
+    i = 0
+    plt.figure(figsize=(6,3))
+    plt.subplot(1,2,1)
+    plot_image(i, predictions[i], test_labels, test_images)
+    plt.subplot(1,2,2)
+    plot_value_array(i, predictions[i],  test_labels)
+    plt.show()
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    # Plot the first 15 test images, their predicted labels, and the true labels.
+    # Color correct predictions in blue and incorrect predictions in red.
+    num_rows = 5
+    num_cols = 4
+    num_images = num_rows*num_cols
+    plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+    for i in range(num_images):
+        plt.subplot(num_rows, 2*num_cols, 2*i+1)
+        plot_image(i, predictions[i], test_labels, test_images)
+        plt.subplot(num_rows, 2*num_cols, 2*i+2)
+        plot_value_array(i, predictions[i], test_labels)
+    plt.tight_layout()
+    plt.show()
 
-# print(f'Type of train images: {type(train_images)}')
-# print(f'Type of train labels: {type(train_labels)}')
-# print(f'train images shape: {train_images.shape}')
-# print(f'first element of train images: {train_images[0]}')
-# print(f'length of train_labels: {len(train_labels)}')
+def displaySampleData():
+    print(f'Type of train images: {type(train_images)}')
+    print(f'Type of train labels: {type(train_labels)}')
+    print(f'train images shape: {train_images.shape}')
+    print(f'first element of train images:\n{train_images[0]}')
+    print(f'length of train_labels:\n{len(train_labels)}')
+    
+    '''
+    display the first image in train_images GUI
+    '''
+    plt.figure()
+    plt.imshow(train_images[0])
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
 
-# print(f'Type of test images: {type(test_images)}')
-# print(f'first element of test images: {test_images[0]}')
-# print(f'Type of test labels: {type(test_labels)}')
-# print(f'test images shape: {test_images.shape}')
-# print(f'length of train_labels: {len(test_labels)}')
-
-# # '''
-# # display the first image in train_images GUI
-# # '''
-# # plt.figure()
-# # plt.imshow(train_images[0])
-# # plt.colorbar()
-# # plt.grid(False)
-# # plt.show()
-
-# # train_images = train_images / 255.0
-# # test_images = test_images / 255.0
-
-# # plt.figure(figsize=(10,10))
-# # for i in range(25):
-# #     plt.subplot(5, 5, i+1)
-# #     plt.xticks([])
-# #     plt.yticks([])
-# #     plt.grid(False)
-# #     plt.imshow(train_images[i], cmap=plt.cm.binary)
-# #     plt.xlabel(class_names[train_labels[i]])
-# # plt.show()
+    print(f'Type of test images: {type(test_images)}')
+    print(f'first element of test images: {test_images[0]}')
+    print(f'Type of test labels: {type(test_labels)}')
+    print(f'test images shape: {test_images.shape}')
+    print(f'length of train_labels: {len(test_labels)}')
 
 '''
-build the model
-
+build the model:
 layer is the basic building block of neural network
+The Sequential class is used to define a linear stack of network layers
+https://www.kdnuggets.com/2018/06/keras-4-step-workflow.html
 '''
+def buildModel():
+    try:
+        model = tf.keras.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28,28)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(10)
+        ])
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28,28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(10)
-])
+        print(model.summary())
 
-'''
-compile the model
-'''
-model.compile(
-    optimizer = 'adam', 
-    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy']
-)
+        ''' compile the model '''
+        model.compile(
+            optimizer = 'adam', 
+            loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+            metrics=['accuracy']
+        )
 
-'''
-feed data to the model
-'''
-
-model.fit(train_images, train_labels, epochs=10)
+        ''' feed data to the model; tensorFlow 1.0 need to use Session() '''
+        # the number of passes of the entire training dataset the machine learning algorithm has completed
+        model.fit(train_images, train_labels, epochs=10)
+        return model
+    except:
+        print('Bad happened in buildModel')
 
 '''
 evaluate accuracy
 '''
-test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 2)
-print(f'\nTest Accuracy: {test_acc}, Test loss: {test_loss}')
+def modelEvaluation(model):
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 2)
+    print(f'\nTest Accuracy: {test_acc}, Test loss: {test_loss}')
 
 '''
 make predictions
 '''
-probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+def makePredictions(model, sample):
+    probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
 
-predictions = probability_model.predict(test_images)
+    predictions = probability_model.predict(sample)
 
-print(f'first image predictions on all 10 labels: {predictions[0]}')
-print(f'highest confidence value of first test image: {np.argmax(predictions[0])}')
+    print(f'first image predictions on all 10 labels: {predictions[0]}')
+    print(f'highest confidence value of first test image: {np.argmax(predictions[0])}')
+    return predictions
 
+def main():
+    displaySampleData()
+    model = buildModel()
+    modelEvaluation(model)
+    predictions = makePredictions(model, test_images)
+    plotAll(predictions)
 
-i = 0
-plt.figure(figsize=(6,3))
-plt.subplot(1,2,1)
-plot_image(i, predictions[i], test_labels, test_images)
-plt.subplot(1,2,2)
-plot_value_array(i, predictions[i],  test_labels)
-plt.show()
-
-# Plot the first X test images, their predicted labels, and the true labels.
-# Color correct predictions in blue and incorrect predictions in red.
-num_rows = 5
-num_cols = 3
-num_images = num_rows*num_cols
-plt.figure(figsize=(2*2*num_cols, 2*num_rows))
-for i in range(num_images):
-  plt.subplot(num_rows, 2*num_cols, 2*i+1)
-  plot_image(i, predictions[i], test_labels, test_images)
-  plt.subplot(num_rows, 2*num_cols, 2*i+2)
-  plot_value_array(i, predictions[i], test_labels)
-plt.tight_layout()
-plt.show()
+if __name__ == "__main__":
+    main()
--- learning_tensorFlow/tutorial01.py
+++ learning_tensorFlow/tutorial01.py
@@ -1,18 +1,78 @@
-import tensorflow.compat.v1 as tf
-'''
-according to tensorflow symbols map 
-https://docs.google.com/spreadsheets/d/1FLFJLzg7WNP6JHODX5q8BDgptKafq_slHpnHVbJIteQ/edit#gid=0
-'''
-tf.disable_v2_behavior()
+import tensorflow as tf
 
+'''
+scalar, vector, matrix and tensor
+a tensor is a vector or a matrix of n-dimensions(ranks) that represents all types of data
+What's a Tensor? https://www.youtube.com/watch?v=f5liqUk0ZTw
+'''
 # initialize two constants
-x1 = tf.constant([1,2,3,4])
-x2 = tf.constant([5,6,7,8])
+c1 = tf.constant([[1,2], [3,4]])
+print(c1)
+
+v1 = tf.Variable([[1,2], [3,4]])
+print(v1)
+
+'''
+tf.float16: 16-bit half-precision floating-point.
+tf.float32: 32-bit single-precision floating-point.
+tf.float64: 64-bit double-precision floating-point.
+tf.bfloat16: 16-bit truncated floating-point.
+tf.complex64: 64-bit single-precision complex.
+tf.complex128: 128-bit double-precision complex.
+tf.int8: 8-bit signed integer.
+tf.uint8: 8-bit unsigned integer.
+tf.uint16: 16-bit unsigned integer.
+tf.uint32: 32-bit unsigned integer.
+tf.uint64: 64-bit unsigned integer.
+tf.int16: 16-bit signed integer.
+tf.int32: 32-bit signed integer.
+tf.int64: 64-bit signed integer.
+tf.bool: Boolean.
+tf.string: String.
+tf.qint8: Quantized 8-bit signed integer.
+tf.quint8: Quantized 8-bit unsigned integer.
+tf.qint16: Quantized 16-bit signed integer.
+tf.quint16: Quantized 16-bit unsigned integer.
+tf.qint32: Quantized 32-bit signed integer.
+tf.resource: Handle to a mutable resource.
+tf.variant: Values of arbitrary types.
+'''
 
 # multiply
-result = tf.multiply(x1,x2)
-print(result)
+result = tf.matmul(c1,v1)
+# result.op is meaningless when eager execution is enabled
+# so uee .numpy
+print(f'\n\n{c1.numpy()}\n*\n{v1.numpy()}\n=\n{result.numpy()}\n\n')
 
-# intialize the session
-with tf.Session() as sess:
-    print(sess.run(result))
+# concatenate by columns
+concat_columns = tf.concat(values=[c1, v1], axis=1)
+print(f'colums concatenantion:\n{concat_columns}\n')
+
+# concatenate by rows
+concat_rows = tf.concat(values=[c1, v1], axis=0)
+print(f'rows concatenation:\n{concat_rows}\n')
+
+
+concat_rows_cast = tf.cast(concat_rows, tf.float32)
+rows, columns = concat_rows_cast.shape
+
+print(concat_rows_cast)
+print(f'# of rows: {rows} and # of columns: {columns}')
+
+concat_rows_cast_reshape = tf.transpose(concat_rows_cast)
+rows, columns = concat_rows_cast_reshape.shape
+
+print(concat_rows_cast_reshape)
+print(f'# of rows: {rows} and # of columns: {columns}')
+
+''' 
+in tensorflow 1.0, a session runs a computational graph
+in tensorflow 2.0, we only use model.fit(...) to let data flow throgh layers
+
+a computational graph is how one 'layer' is transformed to another 'layer'
+layers are different phases of my data
+layers can be sequencial, functional or subclassing which provided by tf.keras (high level API of tf 2.0)
+'''
+
+# available math function in tensorflow
+print(dir(tf.math))
